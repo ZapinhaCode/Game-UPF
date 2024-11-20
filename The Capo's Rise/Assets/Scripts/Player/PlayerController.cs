416dbd08eac5bd23ac79db6db710d2be4f48178a
Bernardo Zaparoli <bernardozaparoli@hotmail.com>
2024-11-20 03:50:31 -0300

    Ajuste tempo de tiro do personagem

--- The Capo's Rise/Assets/Scripts/Player/PlayerController.cs
+++ The Capo's Rise/Assets/Scripts/Player/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded;
     private bool jumpRequested;
     private Animator anim;
+    private bool isShooting = false; // Variável para controlar o estado de tiro
 
     public float speed;
     public float jumpForce;
@@ -29,33 +30,42 @@ public class PlayerController : MonoBehaviour
         {
             jumpRequested = true;
         }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
     }
 
     void FixedUpdate()
     {
         Move();
         HandleJump();
-        Shoot();
     }
 
     void Move()
     {
+        if (isShooting) // Check if the character is shooting
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y); // Stop horizontal movement
+            anim.SetBool("IsRun", false); // Ensure running animation is off
+            return;
+        }
+
         rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
 
-        // Verifica para trocar a posição do personagem 
+        // Handle character orientation and running animation
         if (moveX > 0)
         {
             transform.eulerAngles = new Vector3(0f, 0f, 0f);
             anim.SetBool("IsRun", true);
         }
-
-        if (moveX < 0)
+        else if (moveX < 0)
         {
             transform.eulerAngles = new Vector3(0f, 180f, 0f);
             anim.SetBool("IsRun", true);
         }
-        
-        if (moveX == 0)
+        else
         {
             anim.SetBool("IsRun", false);
         }
@@ -63,7 +73,7 @@ public class PlayerController : MonoBehaviour
 
     void HandleJump()
     {
-        if (jumpRequested)
+        if (jumpRequested && !isShooting)
         {
             if (isGrounded || (currentJumps > 0))
             {
@@ -82,10 +92,16 @@ public class PlayerController : MonoBehaviour
 
     void Shoot()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            anim.Play("Shoot", -1);
-        }
+        isShooting = true; // O personagem está atirando
+        anim.Play("Shoot", -1);
+        StartCoroutine(ShootingCoroutine());
+        anim.SetBool("IsRun", false);
+    }
+
+    IEnumerator ShootingCoroutine()
+    {
+        yield return new WaitForSeconds(0.2f); // Duração da animação de tiro
+        isShooting = false; // O personagem terminou de atirar
     }
 
     void OnCollisionEnter2D(Collision2D collision)
