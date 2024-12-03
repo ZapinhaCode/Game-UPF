217b030581f915c34d2fa246c3c0703f20583e2e
Bernardo Zaparoli <bernardozaparoli@hotmail.com>
2024-12-02 21:18:42 -0300

    Ajustes gerais

--- The Capo's Rise/Assets/Scripts/GameScore.cs
+++ The Capo's Rise/Assets/Scripts/GameScore.cs
@@ -8,8 +8,6 @@ public class GameScore : MonoBehaviour
     public PlayerController player;
     public TextMeshProUGUI textLife;
 
-
-    // Update is called once per frame
     void Update()
     {
         textLife.text = player.life.ToString();
